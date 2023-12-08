58c40ea87a1e236e323b2b1bf083300639f070b1
Tyler Pooschke <tyler.pooschke@pm.me>
2023-12-07 21:20:34 -0600

    init commit

---
+++ src/main/java/petstore/entity/Employee.java
@@ -0,0 +1,32 @@
+package petstore.entity;
+
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import lombok.Data;
+
+@Entity
+@Data
+public class Employee {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long employeeId;
+	private String employeeFirstName;
+	private String employeeLastName;
+	
+	@Column(unique = true)
+	private String employeePhone;
+	private String employeeJobTitle;
+	
+	
+	@ManyToOne(cascade = CascadeType.ALL)
+	@JoinColumn(name = "pet_store_id")
+	private PetStore petStore;
+	
+}
